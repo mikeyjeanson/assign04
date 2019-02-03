3103748402a739a197fc80e80274ab6f9abdbe88
mikeyjeanson <46908977+mikeyjeanson@users.noreply.github.com>
2019-02-03 12:27:29 -0700

    Returns updated

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -2,19 +2,38 @@ package assign04;
 
 public class AnagramChecker{
 
+	/**
+	 * 
+	 * @param filename
+	 * @return String that is sorted by lexicography
+	 */
 	public static String sort(String s) {
 	}
 	
+	/**
+	 * 
+	 */
 	public static <T> void insertionSort(T[], Comparator<? super T> c)
 	{
 		
 	}
 	
+	/**
+	 * 
+	 * @param s
+	 * @param r
+	 * @return true if they are anagrams
+	 */
 	public static boolean areAnagrams(String s, String r)
 	{
 		
 	}
 	
+	/**
+	 * 
+	 * @param filename
+	 * @return the String array of one of the largest anagram collections in the file
+	 */
 	public static String[] getLargestAnagramGroup(String filename)
 	{
 		
